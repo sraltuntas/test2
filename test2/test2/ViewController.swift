a7961d8ef1058bb93612cf8ab14bd00b049a5c34
Soner <s.altuntas@hotmail.com.tr>
2017-11-21 21:54:14 +0300

    merhaba 1

---
+++ test2/test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Soner Altuntaş on 21/11/2017.
+//  Copyright © 2017 Soner Altuntaş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
